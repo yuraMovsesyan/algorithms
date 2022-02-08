336fcf1bf76dc8bdbb4cdaa1e4f6f73ee382b7d6
Yura Movsesyan <56353539+yuraMovsesyan@users.noreply.github.com>
2022-02-08 23:48:58 +0300

    added Quick sort

--- Algorithms/Algorithms/Sort.cs
+++ Algorithms/Algorithms/Sort.cs
@@ -26,5 +26,32 @@
 
             return list;
         }
+
+        public static List<int> Quick(List<int> list)
+        {
+            if (list.Count < 2)
+                return list;
+
+            List<int> lesser_values = new List<int>();
+            List<int> same_values = new List<int>();
+            List<int> larger_values = new List<int>();
+
+            int pivot = list[0];
+
+            foreach (var elem in list) 
+            {
+                if (elem < pivot)
+                    lesser_values.Add(elem);
+                else if (elem == pivot)
+                    same_values.Add(elem);
+                else
+                    larger_values.Add(elem);
+            }
+
+            List<int> lesser_values_sorted = Quick(lesser_values);
+            List<int> larger_values_sorted = Quick(larger_values);
+
+            return lesser_values_sorted.Union(same_values).Union(larger_values_sorted).ToList();
+        }
     }
 }
