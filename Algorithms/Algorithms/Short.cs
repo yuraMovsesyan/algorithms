d4af3360ecabb8de63bbf7151af7ec06ffead19e
Yura Movsesyan <56353539+yuraMovsesyan@users.noreply.github.com>
2022-02-08 22:00:11 +0300

    added Bubble short

---
+++ Algorithms/Algorithms/Short.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms
+{
+    public static class Short
+    {
+        public static List<int> Bubble (List<int> list)
+        {
+            bool flag;
+            for (int borderRight = list.Count; borderRight > 0; borderRight--)
+            {
+                flag = true;
+
+                for (int i = 0; i < borderRight - 1; i++)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        int temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = temp;
+
+                        flag = false;
+                    }
+                }
+
+                if (flag) break;
+            }
+
+            return list;
+        }
+    }
+}
