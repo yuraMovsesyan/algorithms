7714ea8d737b1c0db1c3b88f98afdf303853c67f
Yura Movsesyan <56353539+yuraMovsesyan@users.noreply.github.com>
2022-02-08 23:49:25 +0300

    added test for quick sort

--- Algorithms/Algorithms.Test/SortTest.cs
+++ Algorithms/Algorithms.Test/SortTest.cs
@@ -19,5 +19,33 @@ namespace Algorithms.Test
             //assert
             CollectionAssert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Quick_NaturalFrom10To0_From0To10returned()
+        {
+            //arrenge
+            List<int> x = new List<int>() { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+            List<int> expected = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //act
+            List<int> actual = Sort.Quick(x);
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Quick_18543_13458returned()
+        {
+            //arrenge
+            List<int> x = new List<int>() { 1, 8, 5, 4, 3 };
+            List<int> expected = new List<int>() { 1, 3, 4, 5, 8 };
+
+            //act
+            List<int> actual = Sort.Quick(x);
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
     }
 }
